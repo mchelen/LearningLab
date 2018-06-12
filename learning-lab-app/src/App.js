a48201ac8fcc19937c9380d5435cc66a6ad5c56c
dolojimz <jimmy.flores999@gmail.com>
2018-06-12 14:36:31 -0400

    clean up repo

--- learning-lab-app/src/App.js
+++ learning-lab-app/src/App.js
@@ -1,14 +1,14 @@
 import React, { Component } from 'react';
 import './App.css';
 
-class App extends Component {
-  state = {users: []}
-
-  componentDidMount() {
-    fetch('/users')
-      .then(res => res.json())
-      .then(users => this.setState({ users }));
-  }
+// class App extends Component {
+//   state = {users: []}
+//
+//   componentDidMount() {
+//     fetch('/users')
+//       .then(res => res.json())
+//       .then(users => this.setState({ users }));
+//   }
 
   render() {
     return (
