c93230b5dc5a95916012613651772e33a96590e8
Emmett <edorlester@stsiinc.com>
2018-06-12 17:53:46 -0400

    made React component to call API

---
+++ learning-lab-app/src/WeatherData.js
@@ -0,0 +1,36 @@
+import React, {Component} from 'react';
+
+var request = require('request');
+var fs = require('fs');
+
+const weatherdefault = {};
+const WeatherContext = React.createContext(weatherdefault);
+
+export default class WeatherData extends Component{
+	constructor(props){
+		super(props);
+		this.state = {
+			weather: null
+		}
+		getWeather((err, res, body) => this.setState({
+			weather: body
+		}));
+	}
+
+	render(){
+		<WeatherContext.Provider value={this.state.weather)>
+			{this.props.children}
+		</WeatherContext.Provider>
+	}
+}
+
+function getWeather(callback){
+	const apikey = fs.readFileSync('../.apikey');
+	const cityid = '4744725';
+	var options = {
+		url: 'http://api.openweathermap.org/data/2.5/weather?id='+cityid+'&units=imperial&APPID='+apikey,
+		json: true
+	};
+
+	request(options, callback);
+}
