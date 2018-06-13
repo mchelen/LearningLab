dffbad5b5dc4161d9264eb2733461c1dbf5abdaf
dolojimz <jimmy.flores999@gmail.com>
2018-06-13 14:54:56 -0400

    Added time component

--- learning-lab-app/src/components/WeatherCard.js
+++ learning-lab-app/src/components/WeatherCard.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import ZipForm from "./WeatherCard/ZipForm";
+import Time from "./WeatherCard/Time";
 import { Card } from 'semantic-ui-react'
 
 const WeatherCard = () => (
@@ -8,9 +9,11 @@ const WeatherCard = () => (
     <Card.Content>
       <Card.Header>City, State</Card.Header>
       <Card.Meta>
-        <span className='time'>Time</span>
+        <span className='time'>
+        <Time />
+        </span>
       </Card.Meta>
-      <Card.Description>Weather in Farenheit</Card.Description>
+        <Card.Description>Weather in Farenheit</Card.Description>
     </Card.Content>
     <Card.Content extra>
       <ZipForm />
