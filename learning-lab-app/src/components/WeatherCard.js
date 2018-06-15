d7d3573f0fe599a8aa810fa828624bfc352117ed
Emmett <edorlester@stsiinc.com>
2018-06-15 12:10:09 -0400

    extract and display temperature

--- learning-lab-app/src/components/WeatherCard.js
+++ learning-lab-app/src/components/WeatherCard.js
@@ -1,28 +1,33 @@
-import React from 'react';
+import React, { Component } from 'react';
+import { Card } from 'semantic-ui-react'
 import ZipForm from "./WeatherCard/ZipForm";
 import Time from "./WeatherCard/Time";
-import { Card } from 'semantic-ui-react'
 
-const WeatherCard = (props) => (
-  <div className = "ui centered card">
-  <Card>
-    <img src="https://placeimg.com/290/290/any" alt="Weather Placeholder"/>
-    <Card.Content>
-      <Card.Header>City, State</Card.Header>
-      <Card.Meta>
-        <span className='time'>
-        <Time />
-        </span>
-      </Card.Meta>
-        <Card.Description>
-        {JSON.stringify(props.data)}
-        </Card.Description>
-    </Card.Content>
-    <Card.Content extra>
-      <ZipForm />
-    </Card.Content>
-  </Card>
-  </div>
-)
+import './WeatherCard.css';
 
-export default WeatherCard;
+export default class WeatherCard extends Component{ 
+    render(){
+        let temperature = this.props.data ? this.props.data.main.temp : null;
+        return (
+            <div className = "ui centered card">
+            <Card>
+                <img src="https://placeimg.com/290/290/any" alt="Weather Placeholder"/>
+                <Card.Content>
+                    <Card.Header>City, State</Card.Header>
+                    <Card.Meta>
+                        <span className='time'>
+                        <Time />
+                        </span>
+                    </Card.Meta>
+                        <Card.Description className='temperature'>
+                        {Math.round(temperature)+'\u00b0F'}
+                        </Card.Description>
+                </Card.Content>
+                <Card.Content extra>
+                    <ZipForm />
+                </Card.Content>
+            </Card>
+            </div>
+        )
+    }
+}
--- learning-lab-app/src/components/WeatherData.js
+++ learning-lab-app/src/components/WeatherData.js
@@ -4,35 +4,35 @@ import WeatherCard from './WeatherCard.js';
 var request = require('request');
 
 export default class WeatherData extends Component{
-	constructor(props){
-		super(props);
-		this.state = {
-			weather: null
-		}
-		this.refreshData = this.refreshData.bind(this);
-		this.refreshData();
-	}
+    constructor(props){
+        super(props);
+        this.state = {
+            weather: null
+        }
+        this.refreshData = this.refreshData.bind(this);
+        this.refreshData();
+    }
 
-	refreshData(){
-		getWeather((err, res, body) => this.setState({
-			weather: body
-		}), this.props.type, this.props.location, this.props.units);
-	}
+    refreshData(){
+        getWeather((err, res, body) => this.setState({
+            weather: body
+        }), this.props.type, this.props.location, this.props.units);
+    }
 
-	render(){
-		return (
-			<WeatherCard data={this.state.weather} />
-		);
-	}
+    render(){
+        return (
+            <WeatherCard data={this.state.weather} />
+        );
+    }
 }
 
 function getWeather(callback, type, location, units){
-	const apikey = '9dd566342853579b7ca76a7f2125741e';
-	var qstring = type+'?'+location.type+'='+location.loc+'&units='+units+'&APPID='+apikey;
-	var options = {
-		url: 'http://api.openweathermap.org/data/2.5/'+qstring,
-		json: true
-	};
+    const apikey = '9dd566342853579b7ca76a7f2125741e';
+    var qstring = type+'?'+location.type+'='+location.loc+'&units='+units+'&APPID='+apikey;
+    var options = {
+        url: 'http://api.openweathermap.org/data/2.5/'+qstring,
+        json: true
+    };
 
-	request(options, callback);
+    request(options, callback);
 }
