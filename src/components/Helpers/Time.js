87f18b2c0cf1d8df2ca8caf7c52ab3de37721499
Emmett <edorlester@stsiinc.com>
2018-06-26 11:21:38 -0400

    minor change to clock

--- src/components/Helpers/Time.js
+++ src/components/Helpers/Time.js
@@ -1,33 +1,35 @@
-import React from 'react'
+import React, { Component } from 'react'
 
-class Time extends React.Component {
-      constructor(props) {
+export default class Time extends Component {
+    constructor(props) {
         super(props);
         this.state = {
-          time: new Date().toLocaleString()
+            time: new Date()
         };
-      }
-      componentDidMount() {
-        this.intervalID = setInterval(
-          () => this.tick(),
-          1000
-        );
-      }
-      componentWillUnmount() {
-        clearInterval(this.intervalID);
-      }
-      tick() {
-        this.setState({
-          time: new Date().toLocaleString()
-        });
-      }
-      render() {
-        return (
-          <p className="App-clock">
-            {this.state.time}.
-          </p>
-        );
-      }
-}
+    }
 
-export default Time;
+    componentDidMount() {
+        this.intervalID = setInterval(
+            () => this.tick(),
+            1000
+        );
+    }
+    
+    componentWillUnmount() {
+        clearInterval(this.intervalID);
+    }
+    
+    tick() {
+        this.setState({
+            time: new Date()
+        });
+    }
+    
+    render() {
+        return (
+            <p className="App-clock">
+                {this.state.time.toLocaleString()}
+            </p>
+        );
+    }
+}
