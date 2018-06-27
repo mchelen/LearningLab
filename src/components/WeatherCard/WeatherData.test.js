6ed7f66ef5e6146596dd93a137463210d491a332
Emmett <edorlester@stsiinc.com>
2018-06-27 13:18:52 -0400

    finished coverage for weather components

--- src/components/WeatherCard/WeatherData.test.js
+++ src/components/WeatherCard/WeatherData.test.js
@@ -19,3 +19,32 @@ it('handles invalid zipcode without crashing', (done) => {
         done();
     }, 3000);
 });
+describe('zip code changes', () => {
+    it('standard change', (done) => {
+        const div = document.createElement('div');
+        let component = ReactDOM.render(<WeatherData type='weather' zipcode='12345' />, div);
+        component.handleZipChange({target: {value: '10001'}});
+        setTimeout(() => {
+            ReactDOM.unmountComponentAtNode(div);
+            done()
+        }, 3000);
+    });
+    it('change to invalid value', (done) => {
+        const div = document.createElement('div');
+        let component = ReactDOM.render(<WeatherData type='weather' zipcode='12345' />, div);
+        component.handleZipChange({target: {value: 'abc'}});
+        setTimeout(() => {
+            ReactDOM.unmountComponentAtNode(div);
+            done()
+        }, 3000);
+    });
+    it('change after being unmounted', (done) => {
+        const div = document.createElement('div');
+        let component = ReactDOM.render(<WeatherData type='weather' zipcode='12345' />, div);
+        ReactDOM.unmountComponentAtNode(div);
+        component.handleZipChange({target: {value: 'abc'}});
+        setTimeout(() => {
+            done()
+        }, 3000);
+    });
+});
