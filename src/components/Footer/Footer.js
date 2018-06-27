7fd7758839544bfa23d4642e35182126f1400633
Emmett <edorlester@stsiinc.com>
2018-06-27 09:46:04 -0400

    cleaned up footer code

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,19 +1,30 @@
-import React, { Component } from 'react'
+import React from 'react'
 
-export default class Footer extends Component {
-    render(){
-        return (
-            <div className="footer">
-            <footer class="pv4 ph3 ph5-m ph6-l mid-gray">
-
-            <small class="f6 db tc">© 2018 <b class="ttu">STSI</b>., All Rights Reserved</small>
-                <div class="tc mt3">
-                    <a href="/language/" title="Language" class="f6 dib ph2 link mid-gray dim">Language</a>
-                    <a href="/terms/"    title="Terms" class="f6 dib ph2 link mid-gray dim">Terms of Use</a>
-                    <a href="/privacy/"  title="Privacy" class="f6 dib ph2 link mid-gray dim">Privacy</a>
-                </div>
-                </footer>
+const Footer = () => (
+    <div className='footer'>
+        <footer className='pv4 ph3 ph5-m ph6-l mid-gray'>
+            <small className='f6 db tc'>
+                © 2018 <b className='ttu'>STSI</b>., All Rights Reserved
+            </small>
+            <div className='tc mt3'>
+                <a
+                    href='/language/'
+                    title='Language'
+                    className='f6 dib ph2 link mid-gray dim'
+                >Language</a>
+                <a
+                    href='/terms/'
+                    title='Terms'
+                    className='f6 dib ph2 link mid-gray dim'
+                >Terms of Use</a>
+                <a
+                    href='/privacy/'
+                    title='Privacy'
+                    className='f6 dib ph2 link mid-gray dim'
+                >Privacy</a>
             </div>
-        );
-    }
-}
+        </footer>
+    </div>
+);
+
+export default Footer;
