9b8755b2c0355539f53599754ad16c5baf68f4ea
dolojimz <jimmy.flores999@gmail.com>
2018-07-02 14:25:49 -0400

    Small about us change

--- src/components/AboutUs/AboutUs.js
+++ src/components/AboutUs/AboutUs.js
@@ -14,7 +14,7 @@ export default class AboutUs extends Component {
                     <h3 class="ui center aligned header">
                         <div class="ui huge header">About</div>
                     </h3>
-                        <p> STSI Learning Lab Web Application </p>
+                        <p>STSI LearningLab is a web application developed by two interns at STSI, Jimmy Flores and Emmett Dorlester, with the mentorship of STSI Employees Kevin Melendez and Ben Morris. The idea of LearningLab is to create a set of web applications to run on a Raspberry Pi, web applications consisting of a Weather App, a Traffic App, and more.</p>
                     </div>
                 <Footer />
             </div>
